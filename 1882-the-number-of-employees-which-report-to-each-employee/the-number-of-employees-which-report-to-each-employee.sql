bb9a7873b879647247bfc4451ce72132fd10feed
Anu734 <2k23.cs2310561@gmail.com>
2025-10-26 22:27:36 +0530

    Time: 1131 ms (5.67%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,15 @@
+SELECT 
+    manager.employee_id,
+    manager.name,
+    COUNT(report.employee_id) AS reports_count,
+    ROUND(AVG(report.age)) AS average_age
+FROM 
+    Employees manager
+JOIN 
+    Employees report
+ON 
+    manager.employee_id = report.reports_to
+GROUP BY 
+    manager.employee_id, manager.name
+ORDER BY 
+    manager.employee_id;
