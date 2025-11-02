e3eb0e19827401130ad867808e1c354b4af75f6d
Anu734 <2k23.cs2310561@gmail.com>
2025-11-02 18:14:28 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (60.74%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int left = 0;
-        int right = height.size() - 1;
-        int leftMax = height[left];
-        int rightMax = height[right];
-        int water = 0;
+        int n = height.size();
+        if(n == 0) return 0;
 
-        while (left < right) {
-            if (leftMax < rightMax) {
-                left++;
-                leftMax = max(leftMax, height[left]);
-                water += leftMax - height[left];
-            } else {
-                right--;
-                rightMax = max(rightMax, height[right]);
-                water += rightMax - height[right];
+        int leftmax = 0, rightmax = 0;
+        int maxheight = height[0], index = 0, water = 0;
+
+        // Find the index of the maximum height bar
+        for(int i = 1; i < n; i++){
+            if(height[i] > maxheight){
+                maxheight = height[i];
+                index = i;
             }
         }
 
-        return water;        
+        // Calculate water on the left side of max bar
+        leftmax = height[0];
+        for(int i = 1; i < index; i++){
+            if(height[i] < leftmax){
+                water += leftmax - height[i];
+            } else {
+                leftmax = height[i];
+            }
+        }
+
+        // Calculate water on the right side of max bar
+        rightmax = height[n-1];
+        for(int i = n-2; i > index; i--){
+            if(height[i] < rightmax){
+                water += rightmax - height[i];
+            } else {
+                rightmax = height[i];
+            }
+        }
+
+        return water;
     }
 };
